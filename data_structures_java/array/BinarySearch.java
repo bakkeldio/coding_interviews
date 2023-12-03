ff72d3b3425720af715a6da604daf03d86d0c778
Bakkeldi <49805354+bakkeldio@users.noreply.github.com>
2023-12-02 23:49:50 -0600

    Update BinarySearch.java

--- data_structures_java/array/BinarySearch.java
+++ data_structures_java/array/BinarySearch.java
@@ -1,1 +1,15 @@
+public int binarySearch(int[] array, int high, int low, int target) {
+    if (high < low) {
+      return -1;
+    }
 
+    int mid = (high + low) / 2;
+
+    if (array[mid] == target) {
+      return mid;
+    }
+    if (array[mid] < target) {
+        return binarySearch(array, high, mid + 1, target); 
+    } 
+    return binarySearch(array, mid - 1, low, target);
+}
