4f7b805656e59bba81e5c366792407c80bb60ec8
Bakkeldi <49805354+bakkeldio@users.noreply.github.com>
2024-02-06 18:29:06 -0800

    Time: 2 ms (78.20%) | Memory: 43.3 MB (46.38%) - LeetSync

--- Top_Interview_150_problems/1547-destination-city/destination-city.java
+++ Top_Interview_150_problems/1547-destination-city/destination-city.java
@@ -3,15 +3,11 @@ class Solution {
 
         HashMap<String, String> map = new HashMap<>();
 
-        Set<String> set = new HashSet<>();
-
-
         for (List<String> path: paths) {
             map.put(path.get(0), path.get(1));
-            set.add(path.get(1));
         }
 
-        for (String dest : set){ 
+        for (String dest : map.values()){ 
             if (!map.containsKey(dest)){
                 return dest;
             }
