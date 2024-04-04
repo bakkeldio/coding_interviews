e4f2d28be5890e6f80fb17332494d47ef43226dc
Bakkeldi <49805354+bakkeldio@users.noreply.github.com>
2024-04-04 11:29:59 -0700

    Time: 1 ms (94.44%) | Memory: 44.6 MB (98.34%) - LeetSync

--- Top_Interview_150_problems/419-battleships-in-a-board/battleships-in-a-board.java
+++ Top_Interview_150_problems/419-battleships-in-a-board/battleships-in-a-board.java
@@ -1,39 +1,37 @@
 class Solution {
-    public int countBattleships(char[][] board)
-    {
-        int m=board.length;
-        int n=board[0].length;
-        boolean[][] vis=new boolean[m][n];
-        int ans=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(vis[i][j]==false && board[i][j]=='X')
-                {
-                    dfs(i,j,board,vis);
-                    ans++;
+    
+    public int countBattleships(char[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+        int count = 0;
+
+        // Mark visited cells to avoid revisiting
+        boolean[][] visited = new boolean[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'X' && !visited[i][j]) {
+                    dfs(board, visited, i, j);
+                    count++; // Increment count after sinking a battleship
                 }
             }
         }
-        return ans;
+
+        return count;
     }
-    
-    public void dfs(int i, int j, char[][] board, boolean[][] vis)
-    {
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length || vis[i][j])
-        {
-            return;
+
+    private void dfs(char[][] board, boolean[][] visited, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'X' || visited[i][j]) {
+            return; // Out of bounds, water, or already visited
         }
-        
-        if(board[i][j]=='.')
-        {
-            return;
-        }
-        
-        vis[i][j]=true;
-        dfs(i,j+1,board,vis);
-        dfs(i+1,j,board,vis);
+
+        visited[i][j] = true; // Mark the cell as visited
+
+        // Recursively sink the battleship in all directions
+        dfs(board, visited, i + 1, j); // Down
+        dfs(board, visited, i - 1, j); // Up
+        dfs(board, visited, i, j + 1); // Right
+        dfs(board, visited, i, j - 1); // Left
     }
 
 }
