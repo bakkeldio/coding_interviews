79c0216ebacd325950344f4415c8f6495d445eeb
Bakkeldi <49805354+bakkeldio@users.noreply.github.com>
2024-04-04 11:10:39 -0700

    Time: 1 ms (94.44%) | Memory: 45.2 MB (65.64%) - LeetSync

--- Top_Interview_150_problems/419-battleships-in-a-board/battleships-in-a-board.java
+++ Top_Interview_150_problems/419-battleships-in-a-board/battleships-in-a-board.java
@@ -1,16 +1,26 @@
 class Solution {
     public int countBattleships(char[][] board) {
-        int rows = board.length;
-        int cols = board[0].length;
+        if (board == null || board.length == 0) {
+            return 0;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
         int count = 0;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (board[i][j] == 'X' && 
-                    (i == 0 || board[i - 1][j] != 'X') && 
-                    (j == 0 || board[i][j - 1] != 'X')) {
-                    count++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                    continue;
                 }
+                // We only count the "head" of the battleship
+                if (i > 0 && board[i - 1][j] == 'X') {
+                    continue;
+                }
+                if (j > 0 && board[i][j - 1] == 'X') {
+                    continue;
+                }
+                count++;
             }
         }
 
