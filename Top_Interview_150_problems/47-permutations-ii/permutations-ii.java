5f4adb959ed72efcf26c70350c2ddf404a680120
Bakkeldi <49805354+bakkeldio@users.noreply.github.com>
2024-04-26 12:31:15 -0700

    Time: 2 ms (79.56%) | Memory: 44.7 MB (59.11%) - LeetSync

--- Top_Interview_150_problems/47-permutations-ii/permutations-ii.java
+++ Top_Interview_150_problems/47-permutations-ii/permutations-ii.java
@@ -1,28 +1,27 @@
 class Solution {
     
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> results = new ArrayList<>();
-        Arrays.sort(nums); // Essential for duplicate handling
-        backtrack(nums, results, new ArrayList<>(), new boolean[nums.length]);
-        return results;
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sorting helps identify duplicates
+        backtrack(nums, new boolean[nums.length], result, new ArrayList<>());
+        return result;
     }
 
-    private void backtrack(int[] nums, List<List<Integer>> results, List<Integer> current, boolean[] used) {
+    private void backtrack(int[] nums, boolean[] used, List<List<Integer>> result, List<Integer> current) {
         if (current.size() == nums.length) {
-            results.add(new ArrayList<>(current));
+            result.add(new ArrayList<>(current));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) {
+            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) { 
                 continue; // Skip duplicates
             }
             used[i] = true;
             current.add(nums[i]);
-            backtrack(nums, results, current, used);
-            used[i] = false;
+            backtrack(nums, used, result, current);
             current.remove(current.size() - 1);
+            used[i] = false;
         }
     }
-
 }
